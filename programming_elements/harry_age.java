90b234bc763681e5fd2c6c29058925c542fd1fa7
Rishabh16092004 <rishabhjaiswal16092004@gmail.com>
2025-08-13 11:36:40 +0530

    Initial commit

--- programming_elements/harry_age.java
+++ programming_elements/harry_age.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class harry_age {
+    public static void main(String[] args) {
+        int harry_birth_year = 2000;
+        int curr_year = 2024;
+        int age = curr_year - harry_birth_year;
+        System.out.println("Harry Current age is " + age);
+
+    }
 }
