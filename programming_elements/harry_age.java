4abaa6b7ec15af0d7bb118e8594c5f5addf82b64
Rishabh16092004 <rishabhjaiswal16092004@gmail.com>
2025-09-02 22:54:38 +0530

    Removed folder from repo but kept locally

--- programming_elements/harry_age.java
+++
@@ -1,9 +0,0 @@
-public class harry_age {
-    public static void main(String[] args) {
-        int harry_birth_year = 2000;
-        int curr_year = 2024;
-        int age = curr_year - harry_birth_year;
-        System.out.println("Harry Current age is " + age);
-
-    }
-}
--- programming_elements/level1_2.java
+++
@@ -1,9 +0,0 @@
-public class level1_2 {
-    public static void main(String[] args) {
-        double maths_marks = 94;
-        double physics_marks = 95;
-        double chemistry_marks = 96;
-        double average_percent_marks = (maths_marks + physics_marks + chemistry_marks) / 3;
-        System.out.println("Sam’s average mark in PCM is " + average_percent_marks +"%");
-    }
-}
